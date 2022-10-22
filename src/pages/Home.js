1baf5d98dad66d506b1df7d3ddd08781bb6bfd57
Manidoux41 <110053179+Manidoux41@users.noreply.github.com>
2022-10-22 09:37:00 +0200

    modif home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -4,6 +4,7 @@ const Home = () => {
     return (
         <div>
             <h2>Accueil</h2>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, provident cupiditate eius placeat tempore ullam dolorem doloremque dicta, delectus debitis aliquid sed vitae, id sapiente.</p>
         </div>
     );
 };
