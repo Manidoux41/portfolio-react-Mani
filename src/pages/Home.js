13b7ad1f483db49af93ed92db3745d4979472376
Manidoux41 <110053179+Manidoux41@users.noreply.github.com>
2022-10-22 09:46:04 +0200

    add css properties on home

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -2,7 +2,7 @@ import React from 'react';
 
 const Home = () => {
     return (
-        <div>
+        <div className='home'>
             <h2>Accueil</h2>
             <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolor, provident cupiditate eius placeat tempore ullam dolorem doloremque dicta, delectus debitis aliquid sed vitae, id sapiente.</p>
         </div>
